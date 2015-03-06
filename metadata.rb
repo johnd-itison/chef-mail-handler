e3d8868d88458dc7520a681b96e2dbae0e347256
Lloyd Philbrook <lloyd@tablexi.com>
2015-03-06 14:48:32 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "sysadmins@tablexi.com"
 license          "Apache 2.0"
 description      "Installs/Configures chef-mail-handler gem"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 depends 'chef-client'
 depends 'chef_handler'
