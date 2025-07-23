cde8b384dca46168845f5d0bc408b044ac032c96
John Daniels <john.daniels@itison.com>
2025-07-23 16:58:57 +0100

    Remove deprecated cookbook dep

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email "sysadmins@tablexi.com"
 license          "Apache 2.0"
 description      "Installs/Configures chef-mail-handler gem"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          "0.5.0"
 
 depends 'chef-client'
-depends 'chef_handler'
